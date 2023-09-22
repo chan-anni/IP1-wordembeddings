ad9b6083070acb373b1b49d895826f59c3e19cbb
chan-anni <119639326+chan-anni@users.noreply.github.com>
2023-09-22 10:44:30 -0700

    word2vec pretrained model

--- word2vec.py
+++ word2vec.py
@@ -1,11 +1,14 @@
+
 import gensim
+from gensim.models import Word2Vec
 
 # Load the pretrained Word2Vec model
 model_path = "/Users/anni/Library/CloudStorage/OneDrive-EastsidePreparatorySchool/High School/12th Grade/IP/GoogleNews-vectors-negative300.bin"
 model = gensim.models.KeyedVectors.load_word2vec_format(model_path, binary=True)
 
+
 # Find similar words
-word = 'computer'
+word = 'cat'
 similar_words = model.most_similar(word, topn=5)
 
 print(f"Words similar to {word}:")
