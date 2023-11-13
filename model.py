b512c80311e2873eb8fe60cac8aff1f115058c76
chan-anni <119639326+chan-anni@users.noreply.github.com>
2023-11-12 21:44:14 -0800

    annotating + cleaning code

--- model.py
+++ model.py
@@ -7,11 +7,14 @@ import matplotlib.pyplot as plt
 from scipy.spatial.distance import cosine
 
 
-# Make sure you've downloaded the weights and options files
+# model weight files from https://allenai.org/allennlp/software/elmo
 options_file = "https://allennlp.s3.amazonaws.com/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json"
 weight_file = "https://allennlp.s3.amazonaws.com/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
 
+# this is the model
+# https://saturncloud.io/glossary/elmo/ code help
 elmo = Elmo(options_file, weight_file, 1, dropout=0)
+
 
 def get_elmo_embeddings(sentences):
     character_ids = batch_to_ids(sentences)
@@ -29,48 +32,51 @@ embeddings = get_elmo_embeddings(sentences)
 
 
 # Taking the mean of embeddings of all words in a sentence to get a sentence vector
-# The closer to one the closer the semantics are
+# The closer to 1 the closer the semantics are, closer to -1 is opposite, 0 is just no semantic similarity
 sentence1_vector = embeddings[0].mean(dim=0).numpy()
 sentence2_vector = embeddings[1].mean(dim=0).numpy()
 
 similarity = compute_cosine_similarity(sentence1_vector, sentence2_vector)
 
-print(f"Cosine Similarity: {similarity}")
+print(f"Cosine Similarity: {similarity} of {sentences[0]} and {sentences[1]}")
 
 
 
-# Sample sentences
-sentences = ["I love programming.", "Coding is fun.", "Python is my favorite language."]
+# Sample sentences 2 for visualization
+# here I just want to see how semantic similarities can be seen throughout sentences
+sentences2 = ["I love programming.", "I hate you.", "I love you.", "The black man was shot", "Martha came to the conclusion that shake weights are a great gift for any occasion.", "She had a difficult time owning up to her own crazy self.", "Help me", "dog", "cat", "She found it hard acknowledging that she was psycho"]
+embeddings2 = get_elmo_embeddings(sentences2)
 
 
+# Averaging word embeddings in each sentence, reducing all of them to one dimension
+sentence_embeddings2 = embeddings2.mean(dim=1).detach().numpy()
 
-# Averaging word embeddings in each sentence
-sentence_embeddings = embeddings.mean(dim=1).detach().numpy()
-
-print("Number of samples:", sentence_embeddings.shape[0])
 
 # Number of samples
-n_samples = sentence_embeddings.shape[0]
+n_samples = sentence_embeddings2.shape[0]
 
-# Set perplexity to a value less than n_samples
-perplexity_value = min(30, n_samples - 1)  # Adjust as needed
+# Set perplexity to a value less than n_samples (otherwise throws error)
+perplexity_value = min(30, n_samples - 1) 
 
+# reducing down to 2 dimensions with TSNE so that I can plot
 tsne = TSNE(n_components=2, perplexity=perplexity_value, random_state=0)
-reduced_embeddings = tsne.fit_transform(sentence_embeddings)
+reduced_embeddings = tsne.fit_transform(sentence_embeddings2)
 
 
 x = reduced_embeddings[:, 0]
 y = reduced_embeddings[:, 1]
 
-# Scatter plot
-plt.scatter(x, y)
 
-# Adding labels
-for i, txt in enumerate(sentences):  # 'sentences' is your list of original sentences
-    plt.annotate(txt, (x[i], y[i]))
+# Print out every sentences location as (x,y) coords -> for loop to iterate through sentence array
+for i in range(len(x)):  
+            print(f"Sentence {i+1}: {sentences2[i]}, x: {x[i]}, y: {y[i]}")
+     
+# visualizing
 plt.figure(figsize=(12, 8))
+plt.scatter(x,y)
 
 plt.title("t-SNE visualization of ELMo sentence embeddings")
 plt.xlabel("Dimension 1")
 plt.ylabel("Dimension 2")
 plt.show()
+
